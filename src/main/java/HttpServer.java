ce684202797882b3cc6acb5ec5628abb842c70b9
ahmedabdalmageed95 <ahmedabdalmageed95@yahoo.com>
2026-07-15 11:32:49 +0300

    codecrafters submit [skip ci]

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -39,28 +39,9 @@ public class HttpServer {
 	  
 	  private  void sendResponse(Socket socket,String url) throws IOException {
 		  OutputStream outpustStream=socket.getOutputStream();
-	      byte[] responseToBytes=getResponse(url).getBytes(StandardCharsets.UTF_8);
+	      byte[] responseToBytes=new HttpResponse(url).getResponse().getBytes(StandardCharsets.UTF_8);
 	      outpustStream.write(responseToBytes);
 	      outpustStream.flush();
 	  }
 	  
-	  private  String getResponse(String url) {
-		  return "HTTP/1.1 "+ getResponseCodeAndMessage(url)+getResponseBodyIfExists(url);
-	  }
-	  
-	  private String getResponseCodeAndMessage(String url) {
-		  return url.contentEquals("/") || url.startsWith("/echo/")?"200 OK":"404 Not Found";
-	  }
-	  
-	  private String getResponseBodyIfExists(String url) {
-		  final String crlf="\r\n\r\n";
-		  return url.startsWith("/echo/")?getResponseBody(url):crlf;
-	  }
-	  
-	  private String getResponseBody(String url) {
-		  String [] urlParts=url.split("/");
-		  String urlString=urlParts[urlParts.length-1];
-		  Integer urlStringLength=urlString.length();
-		  return "\r\nContent-Type: text/plain\r\nContent-Length: "+urlStringLength+"\r\n\r\n"+urlString;
-	  }
 }
